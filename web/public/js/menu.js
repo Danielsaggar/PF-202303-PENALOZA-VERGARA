ab87fbeaf91407b3c0502195a0ab8336b034de67
Alejandra Vergara <adva.work33@gmail.com>
2023-11-06 16:35:56 -0500

    avances 01

---
+++ web/public/js/menu.js
@@ -0,0 +1,5 @@
+const $openclose = document.getElementsById("icon");
+      $aside = document.getElementById("aside");
+$openclose.addEventListener( "click",()=>{
+    $aside.classList.toggle("desplegar")
+}) ;     
